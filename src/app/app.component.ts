5ad55989f38aaf9b0fdf06ba2770cb4edf251004
Nneka <nnekka@yandex.ru>
2021-06-03 18:05:49 +0300

    Инициализация проекта

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-project';
+  formConfig = [
+    {formControlName: 'name', label: 'Имя', type: 'text'},
+  ]
 }
