03e54062be9c9df6a8556951b03c50dda58331d5
Nneka <nnekka@yandex.ru>
2021-06-04 08:29:49 +0300

    Создала кастомный инпут

--- src/app/test-input/test-input.component.ts
+++ src/app/test-input/test-input.component.ts
@@ -1,15 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, forwardRef, Input, OnInit} from '@angular/core';
+import {ControlValueAccessor, NG_VALUE_ACCESSOR} from '@angular/forms';
 
 @Component({
   selector: 'app-test-input',
   templateUrl: './test-input.component.html',
-  styleUrls: ['./test-input.component.scss']
+  styleUrls: ['./test-input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(() => TestInputComponent),
+      multi: true
+    }
+  ]
 })
-export class TestInputComponent implements OnInit {
+export class TestInputComponent implements ControlValueAccessor {
+
+  // @Input('input') inputRef: ElementRef
+
+  @Input() value: string = 'number'
+  @Input() formControlName: string = 'name'
+  @Input() val: string | number
+
+  onChange: any = (event: any) => { };
+  onTouched: any = () => { };
 
   constructor() { }
 
-  ngOnInit(): void {
+
+  writeValue(obj: any): void {
+   this.val = obj
   }
+  registerOnChange(fn: any): void {
+    this.onChange = fn;
+    console.log(fn)
+  }
+  registerOnTouched(fn: any): void {
+    this.onTouched = fn;
+  }
+  // setDisabledState?(isDisabled: boolean): void {
+  //   this.disabled = isDisabled;
+  // }
+
 
 }
